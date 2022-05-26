5624f076651df27a98e628cd5bcaaab0b17f5a97
灯明 玲音 <30884264+SuzuneToumyou@users.noreply.github.com>
2022-05-26 16:46:29 +0900

    Create sendtest.py

---
+++ sendtest.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*
+
+import pigpio
+import time
+import struct
+
+import serial
+
+import math
+import csv
+
+import numpy as np
+import pathlib as pl
+
+from bitarray import bitarray
+
+ser_v = serial.Serial('/dev/ttyUSB0', 19200, timeout=None)
+
+def senser_get(ser):
+    s_code = b'\x02'
+    s_ID = b'\x00'
+
+    pi = pigpio.pi()
+    addr = 0x0a
+
+    h = pi.i2c_open(1,addr)
+    pi.i2c_write_device(h, [0x4d])
+    time.sleep(2)
+    count, result = pi.i2c_read_device(h,2051)
+
+    time.sleep(2)
+
+    pi.i2c_close(h)
+
+    if count <= 0: #device_error
+    #    pi.i2c_close(h)
+        return (0)
+    else:
+        tP = []
+        #file_name="./outputdata.dat"
+        #fout= open(file_name,"wb")
+
+        readbuff = bytes(result)
+        #print(result)
+        #fout.write(readbuff)
+
+        #hedder
+        #tPTAT = (256*readbuff[1] + readbuff[0]) #/10
+
+        msg = b"\x02"
+        ser.write(msg)
+        ser.write(s_code)
+        ser.write(s_ID)
+
+        #time.sleep(5)
+        tPTAT = (256*readbuff[1] + readbuff[0])
+        msg = tPTAT.to_bytes(2,"little")
+        ser.write(msg)
+
+        for i in range(1024):
+            #if i != 0:
+            tmp = (256*readbuff[i*2+1] + readbuff[i*2]) #/10
+            #print(tmp.to_bytes(2,"little"))
+            msg = tmp.to_bytes(2,"little")
+            ser.write(msg)
+            #time.sleep(1)
+        #print(readbuff[2050].to_bytes(1,"little"))
+        msg = readbuff[2050].to_bytes(1,"little")
+        ser.write(msg)
+        time.sleep(5)
+        msg = b"\x03"
+        ser.write(msg)
+        tP.clear()
+        #pi.i2c_close(h)
+        return(1)
+    #pi.i2c_close(h)
+
+if __name__ == "__main__":
+
+    return_data = senser_get(ser_v)
